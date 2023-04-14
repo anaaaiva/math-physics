72d6a8bc9929b8df7e6716974c1efb1631bfa503
Anastasia <mortem.insomnia@gmail.com>
2023-04-15 01:09:29 +0300

    fixed representation of start conditions
    fixed representation of start conditions in terms of powers

--- solution.py
+++ solution.py
@@ -2,13 +2,16 @@ import heat_equation as he
 import wave_equation as we
 import sympy
 
+#БОЖЕ МОЙ, ЭТА ЧЕРТОВА ФУНКЦИЯ МОЕ СПАСЕНИЕ
+from sympy.simplify.fu import TRpower
+
 def get_result(controller):
-    phi1 = sympy.sympify(controller['phi1'])
-    phi2 = sympy.sympify(controller['phi2'])
+    phi1 = TRpower(sympy.sympify(controller['phi1']))
+    phi2 = TRpower(sympy.sympify(controller['phi2']))
     if controller['f'] == '':
         f = None
     else:
-        f = sympy.sympify(controller['f'])
+        f = TRpower(sympy.sympify(controller['f']))
     a = sympy.sympify(controller['a'])
     l = sympy.sympify(controller['l'])
 
